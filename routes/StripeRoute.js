164105889e918df5e66b8572c7501906b5bf1255
GithubStuf <123167790+GithubStuf@users.noreply.github.com>
2024-03-25 17:53:18 +0100

    Update StripeRoute.js

--- routes/StripeRoute.js
+++ routes/StripeRoute.js
@@ -1,21 +1,6 @@
-const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+const Controller = require('../controllers/Users_Controller')
 
-
-router.post("/payment", (req, res) => {
-    stripe.charges.create({
-        source:req.body.tokenId,
-        amount:req.body.amount,
-        currency: "usd"
-    }, (stripeError, stripeResponse)=>{
-        if(stripeError){
-            res.status(500).json(stripeError)
-        }
-        else{
-            res.status(200).json(stripeResponse)
-        }
-    })
-})
+router.post("/payment", Controller.Payment)
 
 
 

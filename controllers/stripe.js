4741a8b37e4469749e53c1943dce165fa95257da
GithubStuf <123167790+GithubStuf@users.noreply.github.com>
2024-03-25 17:50:16 +0100

    Update stripe.js

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -1,1 +1,14 @@
-
+const Payment = async (req, res) => {
+    stripe.charges.create({
+        source:req.body.tokenId,
+        amount:req.body.amount,
+        currency: "usd"
+    }, (stripeError, stripeResponse)=>{
+        if(stripeError){
+            res.status(500).json(stripeError)
+        }
+        else{
+            res.status(200).json(stripeResponse)
+        }
+    })
+};

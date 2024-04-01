919ed62f81f0e177aab71fa916088a72335f928c
GithubStuf <123167790+GithubStuf@users.noreply.github.com>
2024-04-01 08:19:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ const connectDB = async () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 
 //Routes
